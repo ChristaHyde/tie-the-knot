403d6b33352700c2edc8926eb2b927a56f66e3ac
Christa <christahyde@yahoo.com>
2019-07-22 09:15:49 -0600

    Changes

--- client/src/components/countdown/Moment.js
+++ client/src/components/countdown/Moment.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Clock from './Clock';
-import {Form, FormControl, Button} from 'react-bootstrap';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class Moment extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
 			// deadLine: 'December 31, 2019',
@@ -11,32 +11,32 @@ class Moment extends Component {
 		}
 	}
 
-	changeDeadLine(){
-		console.log('state', this.state);	
-		this.setState({deadLine: this.state.newDeadLine})	
+	changeDeadLine() {
+		console.log('state', this.state);
+		this.setState({ deadLine: this.state.newDeadLine })
 	}
 
 	render() {
 		return (
-			
-				
+
+
 			<div className="App">
 				<div className="content">
 					<div className="App-title">Days until we say......I DO!! {this.state.deadLine}</div>
-					<Clock 
+					<Clock
 						deadLine={this.state.deadLine}
 					/>
 					<Form inline>
 						<FormControl
-							className="Deadline-input" 
+							className="Deadline-input"
 							placeholder='MM/DD/YYYY'
-							onChange={event => this.setState({newDeadLine: event.target.value})} 
+							onChange={event => this.setState({ newDeadLine: event.target.value })}
 						/>
 						<Button onClick={() => this.changeDeadLine()}>Submit</Button>
 					</Form>
-					</div>
 				</div>
-			
+			</div>
+
 		)
 	}
 }
